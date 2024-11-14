dd8d90f218b7f7d94cbd1ac7d59824b9adc55e4b
ratiya <83394470+XQ19@users.noreply.github.com>
2024-11-14 16:00:54 +0000

    Added the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -5,23 +5,21 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
 
     def test_child_ticket_price_C1b1(self):
-        self.assertEqual(self.zoo.get_ticket_price(75), 100)
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Don't waste money on ticket")
 
     def test_child_ticket_price_C1b2(self):
-        self.assertEqual(self.zoo.get_ticket_price(32), 150)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
 
     def test_child_ticket_price_C1b3(self):
-        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
 
     def test_child_ticket_price_C1b4(self):
-        self.assertEqual(self.zoo.get_ticket_price(25), 150)
+        self.assertEqual(self.zoo.get_ticket_price(35), 150)
 
     def test_child_ticket_price_C1b5(self):
-        self.assertEqual(self.zoo.get_ticket_price(16), 100)
+        self.assertEqual(self.zoo.get_ticket_price(65), 100)
     # Add your additional test cases here.
 
 if __name__ == '__main__':
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if age >= 0  and age <= 12:
+        if age < 0  : 
+             return "Don't waste money on ticket"
+        elif age >= 0  and age <= 12:
             return 50
         elif age >= 13 and age <= 20:
             return 100
         elif age >= 21 and age <= 60:
             return 150
-        elif age >= 60:
+        elif age > 60:
             return 100
